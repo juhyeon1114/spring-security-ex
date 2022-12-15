89bd3b6db0acd5f454db43693e0467c3278a0619
juhyeon <juhyeon1114@gmail.com>
2022-12-15 14:38:23 +0900

    webignore

--- src/main/java/security/springsecurityex/security/configs/SecurityConfig.java
+++ src/main/java/security/springsecurityex/security/configs/SecurityConfig.java
@@ -4,12 +4,14 @@ package security.springsecurityex.security.configs;
 import jakarta.websocket.Encoder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,7 +20,6 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    @Order(1)
     public AuthenticationManager authenticationManager(HttpSecurity httpSecurity) throws Exception {
         String password = passwordEncoder().encode("1234");
 
@@ -30,8 +31,15 @@ public class SecurityConfig {
         return auth.build();
     }
 
+    /**
+     * 정적 파일에 대해서는 security 필터들을 거치지 않고 서빙하도록
+     */
     @Bean
-    @Order(2)
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations()));
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests()
